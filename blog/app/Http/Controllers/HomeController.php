1437d73ad154ebe928822076af03a7c2ac3fa98d
infotecbd <mdhossain.odesk@gmail.com>
2023-07-01 01:41:02 +0600

    Update HomeController.php

--- blog/app/Http/Controllers/HomeController.php
+++ blog/app/Http/Controllers/HomeController.php
@@ -1,15 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 
 class HomeController extends Controller
 {
     function home(request $Request){
 
-        $post=DB::table('posts')->where('pageName','=','projects')->first();
-        return view('pages.projects',['seo'=>$seo]);
+        $posts = DB::table('posts')->get();
+     
+            //return $posts;
+
+            foreach($posts as $post){
+             echo $post->post_title;
+        
+             echo $post->post_content;
+            }
+            echo "\n\n";
 
  
 
